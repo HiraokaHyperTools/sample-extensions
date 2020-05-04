4db21f231168d6fc69b8ec4f9143a0027555c253
kenjiuno <ku@digitaldolphins.jp>
2020-05-04 20:47:13 +0900

    Adding filter2.

---
+++ filter2/background.js
@@ -0,0 +1,10 @@
+browser.windows.onCreated.addListener(getEmail);
+
+async function getEmail(window) {
+  if (window.type === "messageDisplay") {
+    var tabs = await browser.tabs.query({ windowId: window.id });
+    var tab = await browser.tabs.get(tabs[0].id);
+    var mailHeader = await browser.messageDisplay.getDisplayedMessage(tabs[0].id);
+    console.log("mailHeader" + JSON.stringify(mailHeader));
+  }
+}
